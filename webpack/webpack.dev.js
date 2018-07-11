d78fbf6f40679c431a6fcada9e285bae799540e1
Deividas Popelskis <deividas.popelskis@gmail.com>
2018-07-11 23:07:03 +0300

    Initial commit

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const srcPath = path.join(__dirname, '../src');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        path.join(srcPath, 'index.jsx')
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: srcPath
+    }
+}
